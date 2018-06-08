9d07304eb67e62538596f4287e1d523559ab1a22
efp <efp@efp.local>
2018-06-08 14:03:12 +0800

    Initial Commit

---
+++ ServerBotDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServerBotDemo
+//
+//  Created by efp on 2018/6/8.
+//  Copyright © 2018 efp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
